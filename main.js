226c4c49726bc83e6251fd55d3ea8e9e5ed0bd0b
Noah Logan <130315077+Logarrithmic@users.noreply.github.com>
2024-11-09 18:59:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const safeSpaceLight = [
+    ["Folsom Library", 42.72935, -73.68254],
+    ["Troy Public Library", 42.72801, -73.69213]
+];
+const safeSpaceSound = [
+    ["Folsom Library", 42.72935, -73.68254],
+    ["Troy Public Library", 42.72801, -73.69213]
+];
 
 function getLocation() {
 if (navigator.geolocation) {
