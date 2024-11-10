f78af23bc1d09ac4c462eb075e753706d73c9352
Noah Logan <130315077+Logarrithmic@users.noreply.github.com>
2024-11-09 19:37:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,15 +7,20 @@ const safeSpaceSound = [
     ["Troy Public Library", 42.72801, -73.69213]
 ];
 
+const lat = 0;
+const lon = 0;
+
 function getLocation() {
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-} else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-}
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
 
 function showPosition(position) {
-x.innerHTML = "Latitude: " + position.coords.latitude + 
-"<br>Longitude: " + position.coords.longitude;
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
 }
